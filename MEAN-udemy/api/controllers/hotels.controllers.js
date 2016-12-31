0e6ec6886903893734903f32fd64041e3b59bd27
Quint436 <quint436@uw.edu>
2016-12-31 15:38:46 -0800

    asdf

--- MEAN-udemy/api/controllers/hotels.controllers.js
+++ MEAN-udemy/api/controllers/hotels.controllers.js
@@ -4,14 +4,17 @@ var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
     console.log("GET the hotels");
+    console.log(req.query);
     res
       .status(200)
       .json( hotelData );
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-    var hotelId = req.params.hotelId;
+    var hotelId = req.params.hotelId; // This comes through the request object
+    var thisHotel = hotelData[hotelId];
     console.log("GET hotelId", hotelId);
     res
       .status(200)
-      .json( hotelData );
+      .json( thisHotel );
+};
