fb790237c284c967e55bbe2a92ebcc5f820cbbf6
Quint436 <quint436@uw.edu>
2017-01-05 14:12:59 -0800

    Angular additonal practice

--- Angular/udemy_angular_practice/app.js
+++ Angular/udemy_angular_practice/app.js
@@ -1,14 +1,19 @@
 
-var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']); // These Dependencies are the list of mudles that myApp uses.
 
-myApp.controller('mainController', function($scope, $log, $filter, $resource) { // Here we can
-
+myApp.controller('mainController', function($scope, $log, $filter, $resource) { // Here we can place the services, and ngular will use Dependency injection
+// We are injecting services into our controller 
+    $scope.name = 'John'; // Setting a value
+    $scope.formattedname = $filter('uppercase')($scope.name); // Using the filter service. Give it a filter, then pass a string.
     console.log($resource);
-    $log.log('Hello.');
-    $log.info('here is some information.');
-    $log.warn('Warning!');
-    $log.debug('Some debug information.');
-    $log.error('This was an error!!!');
+    $log.info($scope.name);// use the log function
+    $log.info($scope.formattedname);
+    // console.log($resource);
+    // $log.log('Hello.');
+    // $log.info('here is some information.');
+    // $log.warn('Warning!');
+    // $log.debug('Some debug information.');
+    // $log.error('This was an error!!!');
 
 });
 
