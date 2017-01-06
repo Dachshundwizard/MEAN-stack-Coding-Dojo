5dfbb7686b099421b89113dd8af0b6886f388520
Quint436 <quint436@uw.edu>
2017-01-06 15:56:57 -0800

    Started myCalculator project

---
+++ Angular/lightbulb_calculator/assets/calculator.js
@@ -0,0 +1,16 @@
+/* JavaScript Document */
+
+// First create an enclosure of an anonymous function
+// This function can exist with other javascripts within the page
+(function(){
+var app = angular.module('myCalculator', []); // Declaring the variable myapp to be 'myCalculator'
+
+app.controller('CalculatorController'); // This is the name of our main controller 
+
+
+
+
+
+
+
+})();
