47a0484c9c0d83cb2a74f66e9e8f0ba7a2d77060
Quint436 <quint436@uw.edu>
2017-01-12 17:29:02 -0800

    Working with callbacks

---
+++ Other/callback/app.js
@@ -0,0 +1,13 @@
+var app = {};
+
+app.add = function(numbers, callback) {
+    var result = 0;
+    if(numbers !== undefined && numbers.length) {
+        for(number in numbers) {
+            result = result + numbers[number];
+        }
+        if(callback !== undefined) {
+            callback(result);
+        }
+    }
+}
--- Other/callbacks_example.js
+++ Other/callbacks_example.js
@@ -1,3 +1,73 @@
+//////// Callback example javascript ////////
+let x = function(){
+    console.log("I am called from inside a function")
+};
+
+let y = function(callback){
+    console.log('do something');
+    callback();
+}
+
+y(x);
+
+////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+
+let add = function(a,b){
+    return a + b
+};
+
+let doWhatever = function(a,b){
+    console.log(`Here are your two numbers back ${a} , ${b}`)
+}
+
+let multiply  = function(a,b){
+    return a * b;
+}
+
+let calc = function(num1, num2, callback){
+    if(typeof callback === "function"){
+        return callback(num1, num2);
+    }
+};
+console.log(calc(2,3, doWhatever));
+
+////////////////////////////////////////////////////////
+////////////////// Practical Example ///////////////////
+////////////////////////////////////////////////////////
+
+var myArr = [{
+    num: 5,
+    str: 'apple'
+}, {
+    num: 7,
+    str: 'cabbage'
+}, {
+    num: 1,
+    str: 'ban'
+}];
+
+myArr.sort(function(val1, val2){
+    if(val1.str > val2.str) {
+        return -1;
+    } else {
+        return 1;
+    }
+});
+
+console.log(myArr);
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+
 console.log("NOW: ");
 console.log("Declaring and assigning variable 'ninja'.");
 var ninja = 'Libby';
