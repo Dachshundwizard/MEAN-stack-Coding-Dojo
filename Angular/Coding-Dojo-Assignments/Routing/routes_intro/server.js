a6baa3a8b94b35cb8e378f43cd7c2e4bb3e241f0
Quint436 <quint436@uw.edu>
2017-01-19 14:47:36 -0800

    getting partials to work

--- Angular/Coding-Dojo-Assignments/Routing/routes_intro/server.js
+++
@@ -1,9 +0,0 @@
-// Standard express setup
-var express = require('express'),
-app = express(),
-path = require('path');
-
-app.use(express.static(path.join(__dirname, './client'))); // When we turn the server on, its going to go look into bower_components
-app.use(express.static(path.join(__dirname, './bower_components'))); // and client and look for index.html file and load it
-
-app.listen(8000, function() {});
