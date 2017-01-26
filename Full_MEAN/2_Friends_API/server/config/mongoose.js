cf297ea3b536c04f71877edaef5c58b07de3dcc0
Quint436 <quint436@uw.edu>
2017-01-25 22:05:10 -0800

    First full MEAN intro

---
+++ Full_MEAN/2_Friends_API/server/config/mongoose.js
@@ -0,0 +1,3 @@
+console.log('loading mongoose connection and model...');
+var mongoose = require('mongoose');
+var fs = require('fs'); // FS module provides ability to read and write files. Reading is how we obtain the content to serve the clients, and writing is how we output content to the client. 
